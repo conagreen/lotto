45cd008bb61ca56126167a8f9703a5552322aafd
conagreen <verycona@gmail.com>
2023-12-09 19:48:45 +0900

    feat: input 값 검증 코드 추가

--- src/main/java/lotto/util/Calculator.java
+++ src/main/java/lotto/util/Calculator.java
@@ -1,8 +1,15 @@
 package lotto.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
+    private static final Pattern ALLOWABLE_RANGE_OF_VALUES = Pattern.compile("[^0-9\\s+\\-*/]");
+
 
     public static long add(String input) {
+        validateInputValue(input);
+
         String[] tokens = input.split(" ");
         long answer = 0;
         for (int i = 0; i < tokens.length; i++) {
@@ -12,6 +19,8 @@ public class Calculator {
     }
 
     public static long subtract(String input) {
+        validateInputValue(input);
+
         String[] tokens = input.split(" ");
         long answer = Long.parseLong(tokens[0]);
         for (int i = 2; i < tokens.length; i++) {
@@ -21,6 +30,8 @@ public class Calculator {
     }
 
     public static long multiply(String input) {
+        validateInputValue(input);
+
         String[] tokens = input.split(" ");
         long answer = Long.parseLong(tokens[0]);
         for (int i = 2; i < tokens.length; i++) {
@@ -30,11 +41,25 @@ public class Calculator {
     }
 
     public static double divide(String input) {
+        validateInputValue(input);
+
         String[] tokens = input.split(" ");
         double answer = Long.parseLong(tokens[0]);
         for (int i = 2; i < tokens.length; i++) {
             answer /= Integer.parseInt(tokens[i++]);
         }
         return answer;
+    }
+
+    private static void validateInputValue(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        Matcher matcher = ALLOWABLE_RANGE_OF_VALUES.matcher(input);
+
+        if (matcher.find()) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/lotto/util/CalculatorTest.java
+++ src/test/java/lotto/util/CalculatorTest.java
@@ -1,9 +1,12 @@
 package lotto.util;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
 
@@ -12,7 +15,7 @@ class CalculatorTest {
     @DisplayName("덧셈 테스트")
     void 덧셈_테스트(String input, long expected) {
         long actual = Calculator.add(input);
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "[{index}] 뺄셈 테스트 {0} = {1}")
@@ -20,7 +23,7 @@ class CalculatorTest {
     @DisplayName("뺄셈 테스트")
     void 뺄셈_테스트(String input, long expected) {
         long actual = Calculator.subtract(input);
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "[{index}] 곱셈 테스트 {0} = {1}")
@@ -28,7 +31,7 @@ class CalculatorTest {
     @DisplayName("곱셈 테스트")
     void 곱셈_테스트(String input, long expected) {
         long actual = Calculator.multiply(input);
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "[{index}] 나눗셈 테스트 {0} = {1}")
@@ -36,6 +39,20 @@ class CalculatorTest {
     @DisplayName("나눗셈 테스트")
     void 나눗셈_테스트(String input, double expected) {
         double actual = Calculator.divide(input);
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("입력 값이 null 또는 빈 공백일 경우 IllegalArgumentException 반환")
+    void 입력_값이_null_또는_빈_공백일_경우_IllegalArgumentException_반환(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Calculator.add(input));
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException 반환")
+    void 사칙연산_기호가_아닌_경우_IllegalArgumentException_반환() {
+        String input = "123 + 456 % 789";
+        assertThatIllegalArgumentException().isThrownBy(() -> Calculator.subtract(input));
     }
 }
