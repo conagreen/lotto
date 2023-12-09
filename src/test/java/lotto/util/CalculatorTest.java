4e8d957eaabe4ab3041b27217aa38b011ba3de4d
conagreen <verycona@gmail.com>
2023-12-09 17:49:40 +0900

    refactor: @ParameterizedTest 의 각 케이스 별로 name 출력될 수 있도록 개선

--- src/test/java/lotto/util/CalculatorTest.java
+++ src/test/java/lotto/util/CalculatorTest.java
@@ -7,15 +7,15 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "[{index}] 덧셈 테스트 {0} = {1}")
     @CsvSource(value = {"5 + 8:13", "5 + 3:8", "5 + 5 + 1:11"}, delimiter = ':')
-    @DisplayName("덧셈 테스트 - 문자열은 공백을 기준으로 숫자와 사칙 연산 기호로 이루어짐")
+    @DisplayName("덧셈 테스트")
     void 덧셈_테스트(String input, long expected) {
         long actual = Calculator.add(input);
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "[{index}] 뺄셈 테스트 {0} = {1}")
     @CsvSource(value = {"5 - 1:4", "3 - 3:0", "5 - 5 - 1:-1"}, delimiter = ':')
     @DisplayName("뺄셈 테스트")
     void 뺄셈_테스트(String input, long expected) {
@@ -23,7 +23,7 @@ class CalculatorTest {
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "[{index}] 곱셈 테스트 {0} = {1}")
     @CsvSource(value = {"5 * 0:0", "3 * 3:9", "5 * 5 * 2:50"}, delimiter = ':')
     @DisplayName("곱셈 테스트")
     void 곱셈_테스트(String input, long expected) {
@@ -31,7 +31,7 @@ class CalculatorTest {
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "[{index}] 나눗셈 테스트 {0} = {1}")
     @CsvSource(value = {"5 / 1:5", "1 / 2:0.5", "12 / 2 / 2:3"}, delimiter = ':')
     @DisplayName("나눗셈 테스트")
     void 나눗셈_테스트(String input, double expected) {
