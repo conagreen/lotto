a85c9b430a904df6ac17290ac99a1364942a56a2
conagreen <verycona@gmail.com>
2023-12-09 17:20:28 +0900

    feat: 계산기 덧셈 구현

--- src/main/java/lotto/util/Calculator.java
+++ src/main/java/lotto/util/Calculator.java
@@ -1,2 +1,13 @@
-package lotto.util;public class Calculator {
+package lotto.util;
+
+public class Calculator {
+
+    public static long add(String input) {
+        String[] tokens = input.split(" ");
+        int answer = 0;
+        for (int i = 0; i < tokens.length; i++) {
+            answer += Integer.parseInt(tokens[i++]);
+        }
+        return answer;
+    }
 }
--- src/test/java/lotto/util/CalculatorTest.java
+++ src/test/java/lotto/util/CalculatorTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class CalculatorTest {
-  
+
+    @ParameterizedTest
+    @CsvSource(value = {"5 + 8:13", "5 + 3:8", "5 + 5 + 1:11"}, delimiter = ':')
+    @DisplayName("덧셈 테스트 - 문자열은 공백을 기준으로 숫자와 사칙 연산 기호로 이루어짐")
+    void 덧셈_테스트(String input, long expected ) {
+        long actual = Calculator.add(input);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
 }
